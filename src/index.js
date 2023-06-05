9725b9bdcc4646afcde54c0b4a70f4b9b65406e6
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-06-05 22:09:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/tweets-app">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
